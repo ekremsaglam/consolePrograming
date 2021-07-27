f5c2bb65312c2e86fff4f247a522e7eb595d0ee4
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-07-27 11:58:09 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace consolePrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("What is your name? : ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your surname? : ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you " + name + " "+surname);
+
+        }
+    }
+}
